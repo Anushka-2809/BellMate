86796d29e358f322e01af25aa55e6adad6662b02
maityanushka60 <maityanushka60@gmail.com>
2025-09-01 20:00:49 +0000

    error

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/welcome_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
